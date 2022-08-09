d6882f5dd00c3682daa0b01cc9360f130a63d643
Daarkaos <sergicaste97@gmail.com>
2022-08-09 18:09:44 +0200

    Push new

---
+++ etherscanextractor.py
@@ -0,0 +1,88 @@
+# File for functions.
+import sys
+import requests
+from bs4 import BeautifulSoup
+from termcolor import colored
+
+##### Function to get the transaction from Etherscan
+def Get_tx_from_etherscan(hashtx):
+    url = "https://etherscan.io/tx/" + hashtx
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    page = requests.get (url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    if soup.title.string == "Etherscan Error Page":
+        print(colored("Could not connect to Etherscan", "red"))
+        sys.exit()
+    print(colored("Getting info from Etherscan...", "green"))
+    return soup
+
+##### Get addr from Etherscan
+def Get_addr_from_etherscan(addrtx):
+    url = "https://etherscan.io/address/" + addrtx
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    page = requests.get (url, headers=headers)
+    soupaddr = BeautifulSoup(page.content, "html.parser")
+    return soupaddr
+
+##### Get contract from Etherscan
+def Get_contract_from_etherscan(addrcontract):
+    url = "https://etherscan.io/bytecode-decompiler?a=" + addrcontract
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    page = requests.get (url, headers=headers)
+    soupcontract = BeautifulSoup(page.content, "html.parser")
+    return soupcontract
+
+##### Function to get the time from Etherscan transaction
+def Get_time_from_tx(EtherHTML):
+    parsetime = EtherHTML.find(id="ContentPlaceHolder1_divTimeStamp")
+    timestamp = parsetime.find("div", class_="col-md-9")
+    timestamp = timestamp.text.replace("\n","")
+    timestamp = timestamp[timestamp.find('(')+1:timestamp.find(')')]
+    return (timestamp)
+
+##### Function to get the from from Etherscan transaction
+#def Get_participants_from_tx(EtherHTML):
+#    fromtx = EtherHTML.find(id="spanFromAdd")
+#    totx = EtherHTML.find(id="contractCopy")
+#    return (fromtx.text, totx.text)
+
+##### Get info from addr
+def Get_info_addr(EtherAddrfromHTML, EtherAddrtoHTML):
+    type_addr_from = EtherAddrfromHTML.title.string
+    type_addr_from = type_addr_from.replace("| Etherscan","").replace("  "," ").replace("\n","").replace("\r","").replace("\t","").replace("Address ","").replace("| ","")
+    type_addr_from = type_addr_from.rstrip(type_addr_from[-1])
+    type_addr_to = EtherAddrtoHTML.title.string
+    type_addr_to = type_addr_to.replace("| Etherscan","").replace("  "," ").replace("\n","").replace("\r","").replace("\t","").replace("Address ","").replace("| ","")
+    type_addr_to = type_addr_to.rstrip(type_addr_to[-1])
+    type_addr_from_contract = EtherAddrfromHTML.find(id="code").text
+    type_addr_to_contract = EtherAddrtoHTML.find(id="code").text
+    return (type_addr_from, type_addr_to, type_addr_from_contract, type_addr_to_contract)
+
+##### Import contract
+def Get_contract_from_addr(EtherContractHTML):
+    contract = EtherContractHTML.find(id="ContentPlaceHolder1_txtByteCode").text
+    return (contract)
+
+##### Tokens transfered
+def Get_tokens_transfered_from_tx(EtherHTML):
+    tokens_transf = []
+    tokens = EtherHTML.find_all(id="wrapperContent")
+    if tokens:
+        if len(tokens) == 2:   
+            tokens = tokens[1].text
+        else:
+            tokens = tokens[0].text
+    else:
+        print(colored("NO tokens detected!", "red"))
+        return ("no")
+    tokens = tokens.replace("To"," To=").replace("From",";From=").replace("For","For=").split(";")
+    for token in tokens:
+        token_from = token[token.find('From=')+6:token.find('To=')]
+        token_to = token[token.find('To=')+4:token.find('For=')]
+        token_for = token[token.find('For=')+5:]
+        dict_tokens = {"from": token_from, "to": token_to, "for": token_for}
+        tokens_transf.append(dict_tokens)
+
+    del tokens_transf[0]
+    return (tokens_transf)
+
---
+++ parseether.py
@@ -0,0 +1,100 @@
+# MSC
+# Import data from Ethereum TX.
+import argparse
+import colorama
+import sys
+import json
+from config import EtherKey, AlchemyKey
+from etherscan import Etherscan
+from web3 import Web3, HTTPProvider
+from etherscanextractor import *
+from termcolor import colored
+
+##### Declare function to define command-line arguments.
+def readOptions(args=sys.argv[1:]):
+  parser = argparse.ArgumentParser(description="The parsing commands lists.")
+  parser.add_argument("-t", "--tx", help="Type the transaction hash.")
+  opts = parser.parse_args(args)
+  return opts 
+
+# Call functions to read arguments
+options = readOptions(sys.argv[1:])
+hashtx = sys.argv[2]
+
+#Get data from Web3:
+apiKey = AlchemyKey  # From Alchemy
+eth = Etherscan(EtherKey)  # From Etherscan
+web3 = Web3(Web3.HTTPProvider('https://eth-mainnet.alchemyapi.io/v2/'+apiKey))
+try:
+    transaction = web3.eth.get_transaction(hashtx)
+except:
+    print(colored('Unable to get information about indicated hash', 'red'))
+    print(colored('Check the indicated hash and the API key.', 'red'))
+    sys.exit()
+
+print(colored('Getting info about the transaction...', 'green'))
+tx_info_clean = {}
+tx_info_clean['link'] = "https://etherscan.io/tx/" + hashtx
+for element in transaction:
+    if element == 'hash':
+        value = hashtx
+    else:
+        value = transaction[element]
+    tx_info_clean[element] = value
+del tx_info_clean['r']
+del tx_info_clean['s']
+del tx_info_clean['blockHash']
+from_clean = tx_info_clean['from']
+to_clean = tx_info_clean['to']
+
+#Execute functions from local file.
+EtherHTML = Get_tx_from_etherscan(hashtx = hashtx)
+time = Get_time_from_tx(EtherHTML = EtherHTML)
+EtherAddrfromHTML = Get_addr_from_etherscan(addrtx = tx_info_clean['from'])
+EtherAddrtoHTML = Get_addr_from_etherscan(addrtx = tx_info_clean['to'])
+info_addr = Get_info_addr(EtherAddrfromHTML = EtherAddrfromHTML, EtherAddrtoHTML = EtherAddrtoHTML)
+
+# Getting info from internal tx
+try:
+    tx_internal = eth.get_internal_txs_by_txhash(txhash = hashtx)
+    timestamp = tx_internal[0]['timeStamp']
+    for element in tx_internal:
+        del element['timeStamp']
+        del element['blockNumber']
+    tx_info_clean['internal_tx'] = tx_internal
+    tx_info_clean['timestamp'] = timestamp
+except:
+    zero = "zero"
+
+#Parse data
+tx_info_clean['from'] = info_addr[0]
+tx_info_clean['to'] = info_addr[1]
+tx_info_clean['time'] = time
+
+# Getting info from tokens transfered
+tokens = Get_tokens_transfered_from_tx(EtherHTML = EtherHTML)
+tx_info_clean['tokens'] = tokens
+
+# Test if contract exist and write it into a file.
+if "Decompile ByteCode" in info_addr[2]:
+    EtherHTML_from_contract = Get_contract_from_etherscan(addrcontract = from_clean)
+    contract1 = Get_contract_from_addr(EtherContractHTML = EtherHTML_from_contract)
+    f = open("webserver/contracts/" + from_clean + ".bytecode", "w")
+    f.write(contract1)
+    f.close()
+    print(colored('CONTRACT \'FROM\' detected, downloading...!', 'green'))
+
+elif "Decompile ByteCode" in info_addr[3]:
+    EtherHTML_from_contract = Get_contract_from_etherscan(addrcontract = to_clean)
+    contract2 = Get_contract_from_addr(EtherContractHTML = EtherHTML_from_contract)
+    f = open("webserver/contracts/" + to_clean + ".bytecode", "w")
+    f.write(contract2)
+    f.close()
+    print(colored('CONTRACT \'TO\' detected, downloading...!', 'green'))
+
+else:
+    print(colored('NO contract detected!', 'red'))
+
+#tx_info_clean_json = json.dumps(tx_info_clean)
+with open("webserver/json/" + hashtx + '.json', 'w') as json_file:
+  json.dump(tx_info_clean, json_file)
